af7a11479e32939e92ec0c8b554ca03648055a99
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-19 15:44:12 +0300

    select from mysql

--- src/index.js
+++ src/index.js
@@ -33,23 +33,49 @@ function dbDisconnect() {
 
 function dbQuery() {
     return new Promise((resolve, reject) => {
-        conn.query(...arguments, (err, results, fields)=> {
+        conn.query(...arguments, (err, results, fields) => {
             if (err) {
                 reject(err);
                 return;
             }
-            let args = {results, fields};
+            let args = { results, fields };
             resolve(args);
         });
     });
 }
 
+function printTable(rez) {
+    let text = "";
+    for (const col of rez.fields) {
+        text += col.name + "\t";
+    }
+    console.log(text);
+    for (const row of rez.results) {
+        text + "";
+        for (const col of rez.fields) {
+            text += row[col.name] + "\t";
+        }
+        console.log(text);
+    }
+}
+
 
 try {
     await dbConnect();
-    let rez = await dbQuery("select 1 as vienas");
-    console.log(rez);
-    console.log("prisijungiau");
+    let rez = await dbQuery("select * from zmones");
+    // console.log(rez);
+    // console.log("prisijungiau");
+    printTable(rez);
+    console.log("--------------------------------");
+    rez = await dbQuery("select * from kontaktai");
+    printTable(rez);
+    console.log("--------------------------------");
+    rez = await dbQuery(`
+    select vardas, pavarde, tipas, reiksme
+    from zmones left join kontaktai on zmones.id = kontaktai.zmones_id
+    order by vardas, pavarde
+    `);
+    printTable(rez);
 } catch (err) {
     console.log("Klaida: ", err);
 } finally {
